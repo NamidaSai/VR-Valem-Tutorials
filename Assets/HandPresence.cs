2ef94009129f42801a2cc220b78c14298da18905
NamidaSai <timeveilprod@gmail.com>
2020-09-03 15:04:03 +0100

    Initial commit

---
+++ Assets/HandPresence.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandPresence : MonoBehaviour
+{
+    private InputDevice targetDevice;
+
+    void Start()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
+        InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, devices);
+
+        foreach (InputDevice device in devices)
+        {
+            Debug.Log(device.name + device.characteristics);
+        }
+
+        if (devices.Count > 0)
+        {
+            targetDevice = devices[0];
+        }
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
