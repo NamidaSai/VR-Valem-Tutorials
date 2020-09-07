10eac310a9e3e59f4db1d41cb19d420856b62c18
NamidaSai <timeveilprod@gmail.com>
2020-09-07 14:43:34 +0100

    Continuous Movement

---
+++ Assets/ContinuousMovement.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ContinuousMovement : MonoBehaviour
+{
+    public float speed;
+    public XRNode inputSource;
+    public float gravity = -9.81f;
+    public LayerMask groundLayer;
+    public float additionalHeight = 0.2f;
+
+    private float fallingSpeed;
+    private XRRig rig;
+    private Vector2 inputAxis;
+    private CharacterController character;
+    
+    void Start()
+    {
+        character = GetComponent<CharacterController>();
+        rig = GetComponent<XRRig>();
+    }
+
+    void Update()
+    {
+        InputDevice device = InputDevices.GetDeviceAtXRNode(inputSource);
+        device.TryGetFeatureValue(CommonUsages.primary2DAxis, out inputAxis);
+    }
+
+    private void FixedUpdate()
+    {
+        CapsuleFollowHeadset();
+
+        Quaternion headYaw = Quaternion.Euler(0, rig.cameraGameObject.transform.eulerAngles.y, 0);
+        Vector3 direction = headYaw * new Vector3(inputAxis.x, 0, inputAxis.y);
+
+        character.Move(direction * Time.fixedDeltaTime * speed);
+
+        bool isGrounded = CheckIfGrounded();
+        if (isGrounded) 
+        {
+            fallingSpeed = 0;
+        }
+        else
+        {
+            fallingSpeed += gravity * Time.fixedDeltaTime;
+        }
+
+        //if movement is jittery, change Fixed Timestep in project settings to be 0.02 / fps of headset. 
+        // ie. Oculus is 90fps => 0.011111111
+
+        character.Move(Vector3.up * fallingSpeed * Time.fixedDeltaTime);
+    }
+
+    void CapsuleFollowHeadset()
+    {
+        character.height = rig.cameraInRigSpaceHeight + additionalHeight;
+        Vector3 capsuleCenter = transform.InverseTransformPoint(rig.cameraGameObject.transform.position);
+        character.center = new Vector3(capsuleCenter.x, character.height / 2 + character.skinWidth, capsuleCenter.z);
+
+    }
+
+    bool CheckIfGrounded()
+    {
+        Vector3 rayStart = transform.TransformPoint(character.center);
+        float rayLength = character.center.y + 0.01f;
+        bool hasHit = Physics.SphereCast(rayStart, character.radius, Vector3.down, out RaycastHit hitInfo, rayLength, groundLayer);
+        return hasHit;
+    }
+}
